5e9589225d4c2cb51dd58fd97533b1181ac23349
SamilaChanu95 <chanukasamila@gmail.com>
2019-09-24 15:17:31 +0530

    Create new empty activity SetupActivity.

--- app/src/main/java/com/example/socialnetwork/SetupActivity.java
+++ app/src/main/java/com/example/socialnetwork/SetupActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
-
-    private
+public class SetupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_setup);
     }
 }
