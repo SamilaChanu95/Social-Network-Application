60fc6fa017696101d66eb455abb00b919eea5325
SamilaChanu95 <chanukasamila@gmail.com>
2019-09-25 23:14:17 +0530

    Save the user information to firebase database account.

--- app/src/main/java/com/example/socialnetwork/SetupActivity.java
+++ app/src/main/java/com/example/socialnetwork/SetupActivity.java
@@ -1,10 +1,23 @@
  package com.example.socialnetwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,18 +26,79 @@ import de.hdodenhof.circleimageview.CircleImageView;
     private EditText UserName, FullName, CountryName;
     private Button SaveInformationButton;
     private CircleImageView ProfileImage;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+
+    String currentUserID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
+
         UserName = (EditText) findViewById(R.id.setup_username);
         FullName = (EditText) findViewById(R.id.setup_full_name);
         CountryName = (EditText) findViewById(R.id.setup_country_name);
         SaveInformationButton = (Button) findViewById(R.id.setup_information_button);
         ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
+        loadingBar = new ProgressDialog(this);
 
+        SaveInformationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                SaveAccountSetupInformation();
+            }
+        });
+    }
+
+    private void SaveAccountSetupInformation() {
+
+        String username = UserName.getText().toString();
+        String fullname = FullName.getText().toString();
+        String country = CountryName.getText().toString();
+
+        if(TextUtils.isEmpty(username)) {
+            Toast.makeText(SetupActivity.this, "Please enter username..", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(fullname)) {
+            Toast.makeText(SetupActivity.this, "Please enter user full name..", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(country)) {
+            Toast.makeText(SetupActivity.this, "Please enter user country name..", Toast.LENGTH_SHORT).show();
+        }
+        else {
+
+            HashMap userMap = new HashMap();
+            userMap.put("username",username);
+            userMap.put("fullname",fullname);
+            userMap.put("country",country);
+            userMap.put("status","Hey there i am using Social Network.");
+            userMap.put("gender","none");
+            userMap.put("dob","none");
+            userMap.put("relationshipstatus","none");
+
+            UsersRef.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+
+                    if(task.isSuccessful()) {
+
+                        Toast.makeText(SetupActivity.this, "Your account is created successfully.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        String message = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Error occured: "+message , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
     }
 }
