5f833f33808ca5fd057454e62a18f210acb5a9b4
SamilaChanu95 <chanukasamila@gmail.com>
2019-09-25 12:54:34 +0530

    Initialize the items.

--- app/src/main/java/com/example/socialnetwork/SetupActivity.java
+++ app/src/main/java/com/example/socialnetwork/SetupActivity.java
@@ -3,12 +3,28 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SetupActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+ public class SetupActivity extends AppCompatActivity {
+
+    private EditText UserName, FullName, CountryName;
+    private Button SaveInformationButton;
+    private CircleImageView ProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        UserName = (EditText) findViewById(R.id.setup_username);
+        FullName = (EditText) findViewById(R.id.setup_full_name);
+        CountryName = (EditText) findViewById(R.id.setup_country_name);
+        SaveInformationButton = (Button) findViewById(R.id.setup_information_button);
+        ProfileImage = (CircleImageView) findViewById(R.id.setup_profile_image);
+
+
     }
 }
